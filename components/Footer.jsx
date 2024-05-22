956f679a845c71289ec308a72dce157cfec151ea
AshokAshok24 <ashok246445@gmail.com>
2024-05-22 18:46:48 +0530

    Footer

---
+++ components/Footer.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <React.Fragment>
+
+            <footer id="footer">
+                <div class="container">
+                    <div class="copyright">
+                        &copy; Copyright <strong><span>iPortfolio</span></strong>
+                    </div>
+                    <div class="credits">
+
+                        Designed by <a href="https://bootstrapmade.com/">Ashok Kumar</a>
+                    </div>
+                </div>
+            </footer>
+        </React.Fragment>
+    )
+}
+
+export default Footer
