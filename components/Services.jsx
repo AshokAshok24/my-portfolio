38fae07c4585fa3f1a750fc26c786530d366c286
AshokAshok24 <ashok246445@gmail.com>
2024-08-24 21:26:54 +0530

    services Add Tech and details

--- components/Services.jsx
+++ components/Services.jsx
@@ -13,40 +13,68 @@ const Services = () => {
 
                     <div class="row">
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up">
-                            <div class="icon"><i class="bi bi-briefcase"></i></div>
-                            <h4 class="title"><a href="">Lorem Ipsum</a></h4>
-                            <p class="description">Voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident</p>
+                            <div class="icon">
+                                <i class="bi bi-briefcase"></i>
+                                <img src="/assets/img/React.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">React JS</a></h4>
+                            <p class="description">
+                                Build dynamic and responsive user interfaces with React JS, leveraging its component-based architecture for efficient and scalable web applications
+                            </p>
                         </div>
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="100">
-                            <div class="icon"><i class="bi bi-card-checklist"></i></div>
-                            <h4 class="title"><a href="">Dolor Sitema</a></h4>
-                            <p class="description">Minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat tarad limino ata</p>
+                            <div class="icon">
+                                <i class="bi bi-card-checklist"></i>
+                                <img src="/assets/img/Next.js.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">Next JS</a></h4>
+                            <p class="description">
+                                Create fast and optimized web applications with Next JS, a powerful React framework that supports server-side rendering and static site generation.                            </p>
                         </div>
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="200">
-                            <div class="icon"><i class="bi bi-bar-chart"></i></div>
-                            <h4 class="title"><a href="">Sed ut perspiciatis</a></h4>
-                            <p class="description">Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur</p>
+                            <div class="icon">
+                                <i class="bi bi-bar-chart"></i>
+                                <img src="/assets/img/Node.js.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">Node JS</a></h4>
+                            <p class="description">
+                                Develop robust and scalable backend solutions with Node JS, utilizing its non-blocking, event-driven architecture to handle high-concurrency applications
+                            </p>
                         </div>
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="300">
-                            <div class="icon"><i class="bi bi-binoculars"></i></div>
-                            <h4 class="title"><a href="">Magni Dolores</a></h4>
-                            <p class="description">Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum</p>
+                            <div class="icon">
+                                <i class="bi bi-binoculars"></i>
+                                <img src="/assets/img/MySQL.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">MySQL</a></h4>
+                            <p class="description">
+                                Design and manage relational databases with MySQL, ensuring data integrity, security, and high performance for complex applications
+                            </p>
                         </div>
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="400">
-                            <div class="icon"><i class="bi bi-brightness-high"></i></div>
-                            <h4 class="title"><a href="">Nemo Enim</a></h4>
-                            <p class="description">At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque</p>
+                            <div class="icon">
+                                <i class="bi bi-brightness-high"></i>
+                                <img src="/assets/img/PostgresSQL.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">PostgreSQL</a></h4>
+                            <p class="description">
+                                Implement advanced database features with PostgreSQL, a powerful and open-source relational database system known for its extensibility and SQL compliance.
+                            </p>
                         </div>
                         <div class="col-lg-4 col-md-6 icon-box" data-aos="fade-up" data-aos-delay="500">
-                            <div class="icon"><i class="bi bi-calendar4-week"></i></div>
-                            <h4 class="title"><a href="">Eiusmod Tempor</a></h4>
-                            <p class="description">Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi</p>
+                            <div class="icon">
+                                <i class="bi bi-calendar4-week"></i>
+                                <img src="/assets/img/MongoDB.svg" alt="" />
+                            </div>
+                            <h4 class="title"><a href="">MongoDB</a></h4>
+                            <p class="description">
+                                Work with MongoDB, a leading NoSQL database, to build flexible and scalable data models, ideal for handling large volumes of unstructured data                            </p>
                         </div>
                     </div>
 
                 </div>
             </section>
-        </React.Fragment>
+        </React.Fragment >
     )
 }
 
