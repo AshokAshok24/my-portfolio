90daf669c1adaaa2c0131e48601e132c07e935ea
AshokAshok24 <ashok246445@gmail.com>
2024-08-24 21:27:10 +0530

    skills added db

--- components/Skills.jsx
+++ components/Skills.jsx
@@ -8,7 +8,7 @@ const Skills = () => {
 
                     <div class="section-title">
                         <h2>Skills</h2>
-                        <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+                        <p>With a strong foundation in both frontend and backend technologies, I bring a versatile skill set to every project. My expertise spans across various programming languages, frameworks, and tools, enabling me to build comprehensive and high-performing applications.</p>
                     </div>
 
                     <div class="row skills-content">
@@ -18,21 +18,42 @@ const Skills = () => {
                             <div class="progress">
                                 <span class="skill">HTML <i class="val">90%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"90%"}} role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "90%" }} role="progressbar" aria-valuenow="100" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
                             <div class="progress">
                                 <span class="skill">CSS <i class="val">80%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"80%"}} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
                             <div class="progress">
                                 <span class="skill">JavaScript <i class="val">90%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"90%"}} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "90%" }} role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">Bootstrap <i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">MySQL <i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">MSSQLSERVER <i class="val">70%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "70%" }} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
@@ -41,23 +62,44 @@ const Skills = () => {
                         <div class="col-lg-6" data-aos="fade-up" data-aos-delay="100">
 
                             <div class="progress">
-                                <span class="skill">React JS <i class="val">80%</i></span>
+                                <span class="skill">Node JS <i class="val">80%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"80%"}} role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">Express JS<i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">React JS<i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
                             <div class="progress">
                                 <span class="skill">Next JS<i class="val">80%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"80%"}} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="90" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
                             <div class="progress">
-                                <span class="skill">Photoshop <i class="val">55%</i></span>
+                                <span class="skill">PostgreSQL <i class="val">80%</i></span>
                                 <div class="progress-bar-wrap">
-                                    <div class="progress-bar" style={{width:"20%"}} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                            </div>
+
+                            <div class="progress">
+                                <span class="skill">MongoDB <i class="val">80%</i></span>
+                                <div class="progress-bar-wrap">
+                                    <div class="progress-bar" style={{ width: "80%" }} role="progressbar" aria-valuenow="55" aria-valuemin="0" aria-valuemax="100"></div>
                                 </div>
                             </div>
 
