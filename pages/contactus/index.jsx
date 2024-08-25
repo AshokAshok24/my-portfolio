01eabaeaddf90f461f2f39e6d88909f0c8169a69
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:44:22 +0530

    contactus page added

---
+++ pages/contactus/index.jsx
@@ -0,0 +1,12 @@
+import Contactus from '@/components/Contactus'
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <Contactus />
+        </>
+    )
+}
+
+export default index
