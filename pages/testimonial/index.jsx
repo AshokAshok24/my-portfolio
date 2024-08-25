2bf72cf38fd5a2c69c7209755bef58a7b9e8d801
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:46:18 +0530

    testimonial page added

---
+++ pages/testimonial/index.jsx
@@ -0,0 +1,12 @@
+import Testimonial from '@/components/Testimonial'
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <Testimonial />
+        </>
+    )
+}
+
+export default index
