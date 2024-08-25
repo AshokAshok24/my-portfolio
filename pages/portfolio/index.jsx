27b6930e55b965fdbb18162429e39354a6629d09
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:45:01 +0530

    page added portfolio

---
+++ pages/portfolio/index.jsx
@@ -0,0 +1,12 @@
+import Portfolio from '@/components/Portfolio'
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <Portfolio />
+        </>
+    )
+}
+
+export default index
