834f30fd482102d62cc226a7e43261a886bb72d5
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:44:40 +0530

    page added facts

---
+++ pages/facts/index.jsx
@@ -0,0 +1,12 @@
+import Facts from '@/components/Facts'
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <Facts />
+        </>
+    )
+}
+
+export default index
