cc7bb42c54a3580bcca77ec56f58443cd121c84b
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:28:52 +0530

    Link tag added

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
 import Footer from './Footer';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Header = () => {
 
+    const router = useRouter()
     const [sidebarVisible, setSidebarVisible] = React.useState(true);
 
     return (
@@ -27,23 +30,24 @@ const Header = () => {
 
                     <nav id="navbar" class="nav-menu navbar">
                         <ul>
-                            <li><a href="#hero" class="nav-link scrollto active"><i class="bx bx-home"></i> <span>Home</span></a></li>
-                            <li><a href="#about" class="nav-link scrollto"><i class="bx bx-user"></i> <span>About</span></a></li>
-                            <li><a href="#skills" class="nav-link scrollto"><i class="bx bx-user"></i> <span>Skills</span></a></li>
-                            <li><a href="#resume" class="nav-link scrollto"><i class="bx bx-file-blank"></i> <span>Resume</span></a></li>
-                            <li><a href="#portfolio" class="nav-link scrollto"><i class="bx bx-book-content"></i> <span>Portfolio</span></a></li>
-                            <li><a href="#services" class="nav-link scrollto"><i class="bx bx-server"></i> <span>Services</span></a></li>
-                            <li><a href="#contact" class="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></a></li>
+
+                            <li><Link href={'/'} className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></Link></li>
+                            <li><Link href="/#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></Link></li>
+                            <li><Link href={'/#skills'} className="nav-link scrollto"><i className="bx bx-user"></i> <span>Skills</span></Link></li>
+                            <li><Link href={'/#resume'} className="nav-link scrollto"><i className="bx bx-file-blank"></i> <span>Resume</span></Link></li>
+                            <li><Link href={'/#portfolio'} className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Portfolio</span></Link></li>
+                            <li><Link href={'/#services'} className="nav-link scrollto"><i className="bx bx-server"></i> <span>Services</span></Link></li>
+                            <li><Link href={'/#contact'} className="nav-link scrollto"><i class="bx bx-envelope"></i> <span>Contact</span></Link></li >
                         </ul>
-                    </nav>
+                    </nav >
 
-                </div>
-            </header>
-
-
+                </div >
+            </header >
 
 
-        </React.Fragment>
+
+
+        </React.Fragment >
     )
 }
 
