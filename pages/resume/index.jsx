1d4fb145b10f58a2b5b3f8848567de5cf16ca092
AshokAshok24 <ashok246445@gmail.com>
2024-08-25 19:44:50 +0530

    page added resume

---
+++ pages/resume/index.jsx
@@ -0,0 +1,12 @@
+import Resume from '@/components/Resume'
+import React from 'react'
+
+const index = () => {
+    return (
+        <>
+            <Resume />
+        </>
+    )
+}
+
+export default index
